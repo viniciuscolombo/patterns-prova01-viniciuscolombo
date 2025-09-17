1c561eb20376360b0da1c3c297d40bdc8cee18d5
Vinicius Teixeira Colombo <vinicius.296922@alunosatc.edu.br>
2025-09-17 19:12:07 -0300

    Subindo 2 patterns

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,31 @@
-console.log("Hello, World! 001");
+// Padrao: Singleton
+
+class Config {
+    constructor() {
+      if (Config.instance) {
+        return Config.instance;
+      }
+      this.settings = {};
+      Config.instance = this;
+    }
+  
+    setConfig(key, value) {
+      this.settings[key] = value;
+    }
+  
+    getConfig(key) {
+      return this.settings[key];
+    }
+  }
+  
+  console.log("--- Exemplo com Singleton ---");
+  
+  const c1 = new Config();
+  c1.setConfig("lang", "pt-BR");
+  console.log("Config 'lang' definida como 'pt-BR' na primeira instância.");
+  
+  const c2 = new Config();
+  console.log("Acessando 'lang' na segunda instância:", c2.getConfig("lang")); 
+  
+  const saoAMesmaInstancia = c1 === c2;
+  console.log(`c1 e c2 são a mesma instância? ${saoAMesmaInstancia}`); 
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,49 @@
-console.log("Hello, World! 002");
+// Padrao: Factory Method
+
+class Bicicleta {
+    move() {
+      return "Pedalando a bicicleta no pátio da UNISATC";
+    }
+  }
+  
+  class Patinete {
+    move() {
+      return "Andando de patinete pelo estacionamento da UNISATC";
+    }
+  }
+  
+  class Onibus {
+    move() {
+      return "Andando de ônibus e chegando na UNISATC";
+    }
+  }
+  
+  class TransporteFactory {
+    static createTransporte(tipo) {
+      switch (tipo) {
+        case "bicicleta":
+          return new Bicicleta();
+        case "patinete":
+          return new Patinete();
+        case "onibus":
+          return new Onibus();
+        default:
+          throw new Error("Tipo de transporte não suportado.");
+      }
+    }
+  }
+  
+  console.log("\n--- Exemplo com Factory Method ---");
+  
+  try {
+    const transporte1 = TransporteFactory.createTransporte("bicicleta");
+    console.log(transporte1.move());
+  
+    const transporte2 = TransporteFactory.createTransporte("onibus");
+    console.log(transporte2.move());
+  
+    const transporte3 = TransporteFactory.createTransporte("patinete");
+    console.log(transporte3.move());
+  } catch (error) {
+    console.error(error.message);
+  }
